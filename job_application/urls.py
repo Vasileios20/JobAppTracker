bc4c6ba07ecf7855f241939944d6548716275cec
kimatron <kimmykim@gmail.com>
2024-10-13 21:15:58 +0100

    Add statistics url :sparkle:

--- job_application/urls.py
+++ job_application/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.job_application, name='job_application'),
     path('filter/', views.filter_jobs, name='filter'),
+    path('statistics/', views.statistics_view, name='statistics'),
 ]

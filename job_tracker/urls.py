d7ca63a5201bcab28dd6f466de36cfeefd8999f5
kimatron <kimmykim@gmail.com>
2024-10-14 16:57:07 +0100

    Add correct path to statistics page :bug:

--- job_tracker/urls.py
+++ job_tracker/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("accounts/", include("allauth.urls")),
     path("job/", include("job_application.urls")),
+    path('statistics/', views.statistics_view, name='statistics'), 
 ]

3ca326387e4e0b611b3929e7b24d5ccdd7f9f3c6
vasileios20 <vasilis_tsim@outlook.com>
2024-10-14 21:14:22 +0100

    Refactor job_application/urls.py to use JobFormView instead of JobApplicationView

--- job_application/urls.py
+++ job_application/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from .views import JobApplicationView
+from .views import JobFormView
 
 urlpatterns = [
     path('', views.job_application, name='job_application'),
-    path('job-form', JobApplicationView.as_view(), name='job_form'),
-
+    path('job-form', JobFormView.as_view(), name='job_form'),
     path('filter/', views.filter_jobs, name='filter'),
     path('statistics/', views.statistics_view, name='statistics'),
 
